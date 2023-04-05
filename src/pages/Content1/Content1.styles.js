32f841a73078477aabf0b6324561a1f0bdaf09a2
rafifandy <rafifmochammad@gmail.com>
2023-04-05 17:34:38 +0700

    Content 1

---
+++ src/pages/Content1/Content1.styles.js
@@ -0,0 +1,70 @@
+
+
+const styles = {
+    container: {
+        height: '331px',
+        boxSizing: "border-box",
+        background: '#FFFFFF',
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    box0: {
+        flex:3,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    box1: {
+        flex:2,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        background: '#157599',
+    },
+    box2: {
+        flex:2,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        background: '#2093BE'
+    },
+    box3: {
+        flex:2,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        background: '#2EB3E6'
+    },
+    title: {
+        fontFamily: 'Quicksand',
+        fontStyle: "normal",
+        fontWeight: "700px",
+        fontSize: "30px",
+        lineHeight: "38px",
+        color: "#3F3F3F",
+        padding: '0 75px'
+    },
+    description: {
+        fontFamily: 'Quicksand',
+        fontStyle: "normal",
+        fontWeight: "400px",
+        fontSize: "20px",
+        lineHeight: "25px",
+        color: "#3F3F3F",
+        textAlign: 'justify',
+        padding: '0 75px'
+    },
+    iconTitle: {
+        fontFamily: 'Quicksand',
+        fontStyle: "normal",
+        fontWeight: "500px",
+        fontSize: "25px",
+        lineHeight: "31px",
+        color: "#FFFFFF",
+    } 
+}
+
+export default styles
