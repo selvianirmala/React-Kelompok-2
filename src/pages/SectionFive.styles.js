771b51ef5502088baaac31b292a9cd91db93faec
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-04-05 17:27:45 +0700

    move style to SectionFive.styles.js & finish section

---
+++ src/pages/SectionFive.styles.js
@@ -0,0 +1,58 @@
+const styles = {
+    container: {
+        width: '1085px',
+        height: '478px',
+        display: 'flex',
+        padding: '40px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBox: 'box-sizing',
+        margin: 'auto'
+    },
+    leftSection: {
+        marginRight: '30px'
+    },
+    title: {
+        fontFamily: 'Quicksand',
+        fontSize: '40px',
+        fontWeight: 700,
+        lineHeigh: '50px'
+    },
+    body: {
+        fontFamily: 'Quicksand',
+        fontSize: '20px',
+        fontWeight: 400,
+        lineHeigh: '25px',
+    },
+    buttonContact: {
+        width: '310px',
+        height: "89px",
+        backgroundColor: '#157599',
+        color: '#fff',
+        fontFamily: 'Quicksand',
+        fontSize: '30px',
+        fontWeight: 700,
+        lineHeigh: '38px',
+        borderRadius: '8px',
+    },
+    buttonConsul: {
+        width: '310px',
+        height: "89px",
+        backgroundColor: '#fff',
+        color: '#157599',
+        fontFamily: 'Quicksand',
+        fontSize: '30px',
+        fontWeight: 700,
+        lineHeigh: '38px',
+        borderRadius: '8px',
+        border: '3px solid #157599',
+        marginLeft: '10px'
+    },
+    image: {
+        width: '207px',
+        height: '207px',
+        marginLeft: '20px'
+    }
+}
+
+export default styles
