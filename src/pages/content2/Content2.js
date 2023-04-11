8cddb11c62cf7b5c3766c482a7b0590f12a71a93
Husen <saidinahusen10.hs@gmail.com>
2023-04-11 09:38:42 +0700

    pisah style

--- src/pages/content2/Content2.js
+++ src/pages/content2/Content2.js
@@ -1,97 +1,7 @@
 import img1 from '../../assets/img1Section2.png'
 import img2 from '../../assets/img2Section2.png'
 import img3 from '../../assets/img3Section2.png'
-
-const styles = {
-    container: {
-        padding: "90px 75px",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: '#F3F3F3'
-    },
-    title: {
-        fontFamily: 'Quicksand',
-        fontStyle: "normal",
-        fontWeight: "400px",
-        fontSize: "30px",
-        lineHeight: "38px",
-        margin: "22px 0",
-        color: "#000",
-        textAlign: "center"
-    },
-    description: {
-        fontFamily: 'Quicksand',
-        fontStyle: "normal",
-        fontWeight: "400px",
-        fontSize: "20px",
-        lineHeight: "38px",
-        margin: "22px 0",
-        color: "#000",
-        textAlign: "center"
-    },
-    flex: {
-        display: "flex",
-        justifyContent: "space-between",
-        margin: "auto 70px"
-    },
-    flexContainer: {
-        textAlign: 'center'
-    },
-    buttonTop: {
-        marginBottom: "12px",
-        width: "106px",
-        height: "34px",
-        borderRadius: "8px",
-        color: "white",
-        border: 0,
-        fontFamily: 'Quicksand',
-        fontStyle: 'normal',
-        fontWeight: 700,
-        fontSize: '12px',
-        lineHeight: '15px'
-    },
-    buttonBottom: {
-        width: '156px',
-        height: '42px',
-        borderRadius: '8px',
-        border: '1px solid #157599',
-
-        backgroundColor: '#FFFFFF',
-        color: '#157599',
-        fontFamily: 'Quicksand',
-        fontStyle: 'normal',
-        fontWeight: 700,
-        fontSize: '18px',
-        lineHeight: '22px',
-        marginRight: '150px'       
-    },
-    card: {
-        paddingTop: 30,
-        width: "369px",
-        height: "461px",
-
-        background: "#EFEFEF",
-        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-        borderRadius: "8px",        
-    },
-    img: {
-        width: "309px",
-        height: "298px"
-    },
-    textP: {
-        textAlign: "left",
-        width: '306px',
-        marginLeft: 30,
-
-        fontFamily: 'Quicksand',
-        fontStyle: 'normal',
-        fontWeight: 500,
-        fontSize: '20px',
-        lineHeight: '25px'
-    },
-
-}
-
+import styles from "./Content2.styles"
 
 const CARD = [
     {bgBtnTop: "#DA2525", img: img1, textP: "Pemberhentian Air Mengalir Sementara Pada Daerah ..."},
@@ -125,7 +35,7 @@ const Content2 = () => {
                         </div>
                     )
                 })}
-                
+
             </div>
         </div>
     )
